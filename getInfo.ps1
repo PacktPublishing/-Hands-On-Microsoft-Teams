133be282590673b4a6a04bd5f536a2ec7325e518
João Ferreira <joao12ferreira@gmail.com>
2020-04-28 23:46:41 +0100

    Delete getInfo.ps1

--- getInfo.ps1
+++
@@ -1,7 +0,0 @@
-﻿$winVersion = Get-WmiObject Win32_OperatingSystem
-$psVersion = Get-Host | Select-Object Version
-
-
-Write-Host "This computer is running:"
-Write-Host ("Windows {0} - Build {1}" -f $winVersion.Version, $winVersion.BuildNumber)
-Write-Host ("PowerShell {0}.{1}" -f $psVersion.Version.Major, $psVersion.Version.Minor)
